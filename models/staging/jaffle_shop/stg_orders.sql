04b3fad4aac7794b553a44a09ade626d5dddc7a1
velumani.jaganathan@mphasis.com <velumani.jaganathan@mphasis.com>
2021-05-29 13:42:30 +0000

    sources

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -4,8 +4,9 @@ select
     id as order_id,
     user_id as customer_id,
     order_date,
-    status
-from raw.jaffle_shop.orders
+    status,
+    _etl_loaded_at
+from {{ source('jaffle_shop','orders') }}
 
 )
 
